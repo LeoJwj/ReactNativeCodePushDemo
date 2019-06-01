ff800c0b1b2c34c3f0f57982e8df3c67e537e340
jwj <530433274@qq.com>
2019-06-01 10:30:47 +0800

    创建Manager类

--- ReactNativeCodePushAndroidDemo/app/src/main/java/com/example/reactnativecodepushandroiddemo/MainActivity.kt
+++ ReactNativeCodePushAndroidDemo/app/src/main/java/com/example/reactnativecodepushandroiddemo/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.reactnativecodepushandroiddemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-//    private var mReactRootView: ReactRootView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ ReactNativeCodePushAndroidDemo/app/src/main/java/com/example/reactnativecodepushandroiddemo/ReactManager.kt
@@ -0,0 +1,5 @@
+package com.example.reactnativecodepushandroiddemo
+
+object ReactManager {
+
+}
